f27894ce95b039a3818a8d5b02b3171e3264109f
Jade Fox <guiltier.gear@gmail.com>
2025-03-05 20:59:19 -0800

    Use polybar ipc

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 # If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar jade 2>&1 | tee -a /tmp/polybar.log &
