3f06ac88b3f1f30c972fa6a8dc881166625ea817
Jade Fox <guiltier.gear@gmail.com>
2025-03-15 18:00:44 -0700

    Add awesomewm config

---
+++ dot_config/awesome/executable_autorun.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Set up monitors
+if [[ $(xrandr -q | grep "HDMI-1-0 connected") ]]; then
+  xrandr --output eDP-1 --primary --auto --pos 1920x0 --rotate normal \
+    --output HDMI-1-0 --auto --pos 0x0 --rotate normal
+else
+  xrandr --output eDP-1 --primary --auto --pos 0x0 --rotate normal
+fi
+
+# Fix incorrect cursor on desktop
+xsetroot -cursor_name left_ptr &
+
+# Start picom
+pgrep picom || picom -b
+
+# Start polkit-gnome
+pgrep -af polkit-gnome-au || exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Start thunar daemon
+pgrep thunar || exec thunar --daemon &
+
+# Start emote
+pgrep emote || exec emote &
+
+# Set the wallpaper
+feh --no-fehbg --bg-fill --randomize "$HOME"/git/wallpapers/* &
+
+# Fix java applications misbehaving
+wmname LG3D
+
+# Ensure clipmenu has the DISPLAY environment variable
+systemctl --user import-environment DISPLAY
