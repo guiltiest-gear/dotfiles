b879a370c91b636077cb8de73cb75e673f604527
Jade Fox <guiltier.gear@gmail.com>
2025-03-24 16:53:50 -0700

    Make any dialog boxes float in awesomewm

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -577,8 +577,15 @@ awful.rules.rules = {
     properties = { floating = true },
   },
 
-  -- Add titlebars to normal clients and dialogs
-  { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } },
+  -- Make dialog boxes float
+  {
+    rule = {
+      type = "dialog",
+    },
+    properties = {
+      floating = true,
+    },
+  },
 
   -- Set Firefox to always map on the tag named "2" on screen 1.
   -- { rule = { class = "Firefox" },
